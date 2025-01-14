0d7583c71507a18da46807a8586635e45a3eb243
Lubangula <danielmayombo47@gmail.com>
2025-01-14 10:56:57 +0000

    correction image

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // Charger l'URI de MongoDB depuis .env
 const mongoURI = process.env.MONGO_URI
 
-// Fonction de connexion à MongoDB
+// Fonction de connexion à MongoDB 
 const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI);
@@ -15,12 +15,12 @@ const connectDB = async () => {
         process.exit(1);  // Quitter l'application en cas d'échec de la connexion
     }
 }; 
- 
+
 module.exports = { connect: connectDB }; 
 
 
 // const mongoose = require('mongoose');
-
+ 
 // // Fonction de connexion à MongoDB
 // const connectDB = async () => {
 //     try {
